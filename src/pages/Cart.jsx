11b42a39fe2ea4d9d85f8c91dc74261d9a26d2ab
Muslim121 <tulegenov_muslim@mail.ru>
2024-02-19 17:19:33 +0500

    dfd

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,61 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
+import { Links } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import styles from "./Cart.module.css";
 
 const Cart = () => {
   return (
-    <div>
-      <h1>Корзина</h1>
+    <div className={styles.cart_all}>
+      <div className={styles.cart_space}>
+        <div className={styles.cart}>
+          <FaShoppingCart className="cart-icon" />
+          <h2>Корзина</h2>
+        </div>
+        <div className={styles.cart_right}>
+          <FaRegTrashAlt className={styles.cart_icon} />
+          <span>Очистить корзину</span>
+        </div>
+      </div>
+      <div>
+        <div className={styles.bezhbarmack_cart}>
+          <img
+            src="https://storage.emenu.delivery/product/images/7r/de/7rdenprfsx1sujgkxmcmfmske5nwbw-e.jpg"
+            alt="bezhbarmack"
+            className={styles.img_cart}
+          />
+          <div className={styles.start_title}>
+            <h3>Бежбармак классический</h3>
+            <p className={styles.text_down}>тонкое тесто, 26см</p>
+          </div>
+          <div className={styles.button_add}>
+            <p className={styles.add}>-</p>
+            <p>2</p>
+            <p className={styles.add}>+</p>
+          </div>
+          <p className={styles.price}>770 ₽</p>
+          <MdCancel className={styles.cancel} />
+        </div>
+        <div className={styles.all}>
+          <h3>
+            Всего <b>1шт</b>
+          </h3>
+          <h3>
+            Сумма заказа: <b>770 ₽</b>
+          </h3>
+        </div>
+        <div className={styles.last}>
+          <div className={styles.button_back}>
+            <IoIosArrowBack />
+            <p>Вернуться назад</p>
+          </div>
+          <div className={styles.button_back}>
+            <p>Оплатить сейчас</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
